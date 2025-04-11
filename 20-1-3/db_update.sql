f10e465e18479e84f9d7bc75872e157594df472d
YYDSSYYDS <690844674@qq.com>
2025-04-12 02:09:06 +0800

    20-1-3 Upload

--- 20-1-3/db_update.sql
+++ 20-1-3/db_update.sql
@@ -2,6 +2,6 @@ ALTER TABLE character_info
 ADD COLUMN lastserver INTEGER DEFAULT 0;
 
 ALTER TABLE guild_matching
-ADD COLUMN introduction_text VARCHAR(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+ADD COLUMN introduction_text VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
 ADD COLUMN MinLevel INT,
 ADD COLUMN MaxLevel INT;
